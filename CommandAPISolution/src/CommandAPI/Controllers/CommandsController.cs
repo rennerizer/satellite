8718922060206fb7c6e25733c767d6cfa63b4a74
Nathan Renner <rennerizer@gmail.com>
2020-10-02 10:26:56 -0600

    Added Commands Controller

---
+++ CommandAPISolution/src/CommandAPI/Controllers/CommandsController.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections.Generic;
+
+using Microsoft.AspNetCore.Mvc;
+
+namespace CommandAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class CommandsController : ControllerBase
+    {
+        [HttpGet]
+        public ActionResult<IEnumerable<string>> Get()
+        {
+            return new string[] { "this", "is", "hard", "coded" };
+        }
+    }
+}
--- CommandAPISolution/src/CommandAPI/Startup.cs
+++ CommandAPISolution/src/CommandAPI/Startup.cs
@@ -16,6 +16,7 @@ namespace CommandAPI
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddControllers();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -30,10 +31,12 @@ namespace CommandAPI
 
             app.UseEndpoints(endpoints =>
             {
-                endpoints.MapGet("/", async context =>
-                {
-                    await context.Response.WriteAsync("Hello World!");
-                });
+                endpoints.MapControllers();
+
+                // endpoints.MapGet("/", async context =>
+                // {
+                //     await context.Response.WriteAsync("Hello World!");
+                // });
             });
         }
     }
