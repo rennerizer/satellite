269b0ab9d1c99182d1f3b20f3ef25f760b8f0916
Nathan Renner <rennerizer@gmail.com>
2020-10-04 13:34:21 -0600

    Added DbContext and migrations

---
+++ CommandAPISolution/src/CommandAPI/Data/CommandRepository.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+using CommandAPI.Models;
+
+namespace CommandAPI.Data
+{
+    public class CommandRepository : IPersistCommand
+    {
+        private readonly CommandContext _context;
+
+        public CommandRepository(CommandContext context)
+        {
+            _context = context;
+        }
+
+        public bool SaveChanges()
+        {
+            throw new NotImplementedException();
+        }
+
+        public IEnumerable<Command> GetAllCommands()
+        {
+            var commands = _context
+                .CommandItems
+                .ToList();
+
+            return commands;
+        }
+
+        public Command GetCommandById(int id)
+        {
+            var command = _context
+                .CommandItems
+                .FirstOrDefault(item => item.Id == id);
+
+            return command;
+        }
+
+        public void CreateCommand(Command command)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void UpdateCommand(Command command)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void DeleteCommand(Command command)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
--- CommandAPISolution/src/CommandAPI/Startup.cs
+++ CommandAPISolution/src/CommandAPI/Startup.cs
@@ -1,5 +1,7 @@
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 
@@ -9,14 +11,27 @@ namespace CommandAPI
 {
     public class Startup
     {
+        public IConfiguration Configuration { get; }
+
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
         // This method gets called by the runtime. Use this method to add services to the container.
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
+            // Command DbContext
+            services.AddDbContext<CommandContext>(options =>
+            {
+                options.UseNpgsql(Configuration.GetConnectionString("PostgreSqlConnection"));
+            });
+
             services.AddControllers();
 
             // Command Repository
-            services.AddScoped<IPersistCommand, MockPersistCommand>();
+            services.AddScoped<IPersistCommand, CommandRepository>();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
