b00b1442c5dc10ccabd310efc2d4b0c1a54d53ad
Nathan Renner <rennerizer@gmail.com>
2020-10-04 12:54:09 -0600

    Added EF Context class

---
+++ CommandAPISolution/src/CommandAPI/Data/CommandContext.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+using CommandAPI.Models;
+
+namespace CommandAPI.Data
+{
+    public class CommandContext : DbContext
+    {
+        public CommandContext(DbContextOptions<CommandContext> options)
+            : base(options)
+        {
+
+        }
+
+        public DbSet<Command> CommandItems { get; set; }
+    }
+}
